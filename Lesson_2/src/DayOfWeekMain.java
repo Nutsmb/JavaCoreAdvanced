22ad621d4e8cabc54fca42c57ea10f6db6f518ae
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-20 08:23:30 +0300

    Add Enum DayOfWeek. Add getWorkingHours method

--- Lesson_2/src/DayOfWeekMain.java
+++ Lesson_2/src/DayOfWeekMain.java
@@ -1,5 +1,17 @@
+import java.time.DayOfWeek;
+
 public class DayOfWeekMain {
+
     public static void main(final String[] args) {
         System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+    }
+
+    public static String getWorkingHours (DayOfWeek day) {
+        if (day.ordinal() < 5) {
+            return ("Осталось " + (5 - day.ordinal()) * 8 + " рабочих часов");
+        }
+        else {
+            return ("Сегодня выходной");
+        }
     }
 }
--- Lesson_2/src/Enum_DayofWeek.java
+++ Lesson_2/src/Enum_DayofWeek.java
@@ -1,2 +1,3 @@
 public enum Enum_DayofWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
