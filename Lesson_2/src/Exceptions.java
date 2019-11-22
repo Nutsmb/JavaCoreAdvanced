8614ebb2963d0d800dd396ec4b841439bba84c8c
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-22 22:25:22 +0300

    Generate array of random numbers

--- Lesson_2/src/Exceptions.java
+++ Lesson_2/src/Exceptions.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Exceptions {
+    public static void main(String[] args) {
+        int N = 4; // тут можно изменить количество ячеек для ловли MyArraySizeException
+        int M = 4; // и тут тоже
+        
+        String[][] Arr = new String[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Arr[i][j] = String.valueOf(Math.round(Math.random() * 10));
+            }
+        }
+    }
 }
