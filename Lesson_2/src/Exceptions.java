e450b09d52396467f7141de214042603e9bf3ce0
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-22 22:28:49 +0300

    Add printArrSum method

--- Lesson_2/src/Exceptions.java
+++ Lesson_2/src/Exceptions.java
@@ -11,6 +11,24 @@ public class Exceptions {
         }
     }
 
+    public static void printArrSum(String[][] arr) throws MyArraySizeException {
+        int sum = 0;
+        if (arr.length != 4 || arr[0].length != 4) throw new MyArraySizeException(arr.length, arr[0].length);
+        else {
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[0].length; j++) {
+                    try {
+                        sum += Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e){
+                        System.out.println("Неверные данные в ячейке "+ i +" " + j);
+                        e.printStackTrace();
+                    }
+                }
+            }
+            System.out.println("Сумма элементов массива " + sum);
+        }
+    }
+
     public static class  MyArraySizeException extends Exception {
         public MyArraySizeException(int rows, int columns){
             int A = 4; // при превышении А строк выкинет исключение
