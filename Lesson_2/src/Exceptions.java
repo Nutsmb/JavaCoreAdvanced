93408e7a6032f76bcbab9e85821f5532add09b21
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-22 22:26:56 +0300

    Add MyArraySizeException

--- Lesson_2/src/Exceptions.java
+++ Lesson_2/src/Exceptions.java
@@ -2,11 +2,25 @@ public class Exceptions {
     public static void main(String[] args) {
         int N = 4; // тут можно изменить количество ячеек для ловли MyArraySizeException
         int M = 4; // и тут тоже
-        
+
         String[][] Arr = new String[N][M];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 Arr[i][j] = String.valueOf(Math.round(Math.random() * 10));
+            }
+        }
+    }
+
+    public static class  MyArraySizeException extends Exception {
+        public MyArraySizeException(int rows, int columns){
+            int A = 4; // при превышении А строк выкинет исключение
+            int B = 4; // при превышении В столбцов выкинет исключение
+            System.out.print("Неверный размер массива. ");
+            if(rows > A){
+                System.out.println("Строк должно быть " + A + ", а не " + rows + ". ");
+            }
+            else if (columns > B){
+                System.out.println( "Cтолбцов должно быть " + B + ", а не " + columns);
             }
         }
     }
