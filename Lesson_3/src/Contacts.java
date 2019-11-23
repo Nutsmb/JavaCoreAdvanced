2548000d67ec528069aa9efeab5fb22282e90503
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-23 22:24:49 +0300

    Add addContact method

--- Lesson_3/src/Contacts.java
+++ Lesson_3/src/Contacts.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Contacts {
+    private static Map<String, ArrayList<String>> contacts = new HashMap<>();
+
+    public static void main(String[] args) {
+    }
+
+    public static void addContact(String name, String tel){
+        if(!contacts.containsKey(name)){
+            contacts.put(name,(new ArrayList()));
+        }
+        contacts.get(name).add(tel);
+    }
 }
