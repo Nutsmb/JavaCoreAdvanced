90c87a841694500042e4f2cd28e955857c44d2e0
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-11-23 22:27:49 +0300

    testing

--- Lesson_3/src/Contacts.java
+++ Lesson_3/src/Contacts.java
@@ -6,6 +6,22 @@ public class Contacts {
     private static Map<String, ArrayList<String>> contacts = new HashMap<>();
 
     public static void main(String[] args) {
+        addContact("Rufus", "89999999999");
+        addContact("Arus", "89992134999");
+        addContact("Gerios", "89569999922");
+        addContact("Rufus", "89991111111");
+        addContact("Awers", "89999966399");
+        addContact("Horius", "89999432099");
+
+        String c1 = "Rufus";
+        String c2 = "Awers";
+
+        getContact(c1);
+        getContact(c2);
+
+        for(Map.Entry<String, ArrayList<String>> item : contacts.entrySet()){
+            getContact(item.getKey());
+        }
     }
 
     public static void addContact(String name, String tel){
