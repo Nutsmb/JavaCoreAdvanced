534c65c86368f9e8b3182ca666dc08cc9b6a16ad
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2019-12-01 15:30:36 +0300

    multithreading implementation

--- Multithreading/src/Threads.java
+++ Multithreading/src/Threads.java
@@ -1,16 +1,57 @@
-public class One_Thread {
-    static final int size = 10000000;
-    static final int h = size / 2;
+public class Threads {
+
+    static final int NUMBER_OF_THREADS = 2;
+    static final int size = 10;//
+    static int h = size/NUMBER_OF_THREADS;
+    static float[] arr = new float[size];
+    static float[] arrEnd = new float[size];
 
     public static void main(String[] args) {
-        float[] arr = new float[size];
+        //float[] arr = new float[size];
         for (int i = 0; i < size; i++) {
             arr[i] = 1;
         }
-        long a = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
-            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arrEnd[i] = 1;
         }
-        System.out.println(System.currentTimeMillis() - a);
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrEnd[i]+" ");
+        }
+        System.out.println();
+        MultiThread();
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrEnd[i]+" ");
+        }
+    }
+
+    public static void MultiThread() {
+        long initTime = System.currentTimeMillis();
+        new Thread(new MyRunnableClass(0)).start();
+        new Thread(new MyRunnableClass(1)).start();
+        System.out.println(System.currentTimeMillis() - initTime);
+    }
+
+    static class MyRunnableClass implements Runnable {
+        float[] _arr = new float[h];
+        int iteration;
+
+        public MyRunnableClass(int n) {
+            System.arraycopy(arr, n*h, _arr, 0, h);
+            iteration = n;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < h; i++) {
+                try {
+                    _arr[i] = (float)(_arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            System.arraycopy(_arr, 0, arrEnd, iteration*h, h);
+        }
     }
 }
+
+
